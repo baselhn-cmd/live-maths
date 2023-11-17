cc40da739bdd2b97ddf69df1deb4850d86b005bf
baselhn-cmd <>
2023-11-17 07:57:04 +0000

    Set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCoorectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer()  {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
